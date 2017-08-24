2cebf4b0098fc4dd432a834d7e03cbc886ca9813
vphriang1 <vphriang21@gmail.com>
2017-08-23 17:46:21 -0700

    program can pay user back in the console according to dice roll

--- public_html/betting.js
+++ public_html/betting.js
@@ -5,132 +5,81 @@
  */
 
 
-/*
-var i, j; //looping
-var bankerfee=0;
 
- // array to track how many times someone gets paid
- var multiply = new Array(6);
- for (i = 0; i < 6; i++) {
- multiply[i] = new Array(2);
- }
- 
- // array to track which player placed money where
- var stored = new Array(6);
- for (i = 0; i < 6; i++) {
- stored[i] = new Array(2);
- }
- 
- 
- //make store and multiply zero
- for(i=0; i<6; i++){
-     for(j=0; j<2; j++){
-      stored[i][j]=0;
-      multiply[i][j]=0;
-      console.log(stored[i][j]);   
-     }
- }
- 
- 
+var i, j, m; //looping
+var bankerfee = 0;
 
- 
- //pretend the dice rolls 3 wines
- var dice = [1,1,1];
-
-//return money
-for(i=0; i<3; i++){
-    switch (dice[i]){
-        case 0:
-            returnMoney(i, 0);
-        break;
-        case 1:
-            returnMoney(i, 1);
-        break;
-        case 2:
-            returnMoney(i, 2);
-        break;
-        case 3:
-            returnMoney(i, 3);
-        break;
-        case 4:
-            returnMoney(i, 4);
-        break;
-        case 5:
-            returnMoney(i, 5);
-        break;
-        default:
-            
-    }
-}//end of for loop
-
-function returnMoney(i, k){
-    for(j =0; j<2; j++){
-        multiply[k][j]++;
-        
-    }
-    if(i==1){
-    if(dice[i]!=dice[0]){
-        allocateMoney(k);
-    }}else if(i == 2){
-    if((dice[i]!=dice[1])&&(dice[i]!=dice[0])){
-        allocateMoney(k);
-    }}else{
-        allocateMoney(k);
-            
-        
-    }
-       
+// array to track how many times someone gets paid
+var multiply = new Array(6);
+for (i = 0; i < 6; i++) {
+    multiply[i] = new Array(2);
 }
 
-function allocateMoney(k){
-    for(j=0; j < 2; j++){
-        //add money
-        $scope.node1 += stored[k][j];
-        bankerfee += stored[k][j];
-    }for(i=0;i<2;i++){
-        $scope.node1 += (store[0][i] * multiply[0][i]) + (store[1][i] * multiply[1][i]) + (store[2][i] * multiply[2][i]) + (store[3][i] * multiply[3][i])
-					+ (store[4][i] * multiply[4][i]) + (store[5][i] * multiply[5][i]);
-    }
-   
+// array to track which player placed money where
+var stored = new Array(6);
+for (i = 0; i < 6; i++) {
+    stored[i] = new Array(2);
 }
-*/
+
+
+//make store and multiply zero
+for (i = 0; i < 6; i++) {
+    for (j = 0; j < 2; j++) {
+        stored[i][j] = 0;
+        multiply[i][j] = 0;
+        //console.log(stored[i][j]);
+    }
+}
+
+
+
+
+
+
+
+var turn = 0;
+var animalposition=0;
 
 var app = angular.module('playerStatus', []);
 app.controller('myCtrl', function ($scope) {
 
     $scope.bet;
     $scope.chosenanimal;
-    $scope.turn = 0;
     $scope.amountofplayers = 0;
     $scope.i = 0; //increments for 'for' loops
     $scope.textfeed;
 
 
+    var node = [0, 1, 2];
+    var firstprompt = 0; // makes sure animal is chosen first
+
     // Person object constructer
     function Person(name, money) {
         this.name = name;
         this.money = money;
-        this.changeMoney = function () {
-            this.money;
-            this.name;
-        };
     }
 
     // player data
-    var node1 = new Person("Wing", 5);
-    //object variables
-    $scope.name = node1.name;
-    $scope.money = node1.money;
+    node[0] = new Person("Wing", 5);
+    node[1] = new Person("Eric", 5);
+    node[2] = new Person("Banker", 0); //easy to calculate and keep track
+    //for loops won't go up to the banker
+
+    //scope variables 
+    $scope.name = node[turn].name;
+    $scope.money = node[turn].money;
     $scope.amountofplayers++;
 
     $scope.promptuser = function () {
         var x, text;
         x = document.getElementById("betposition").value;
-
-        if (isNaN(x) || x < 0 || x > 5) {
+        animalposition = parseInt(x);
+        console.log("The typeof animalposition is: "+ typeof animalposition);
+        if (isNaN(x) || x < 0 || x > 5 || x === "") {
             text = "Error";
         } else {
             text = "Choice: ";
+            firstprompt = 1;
         }
         $scope.chosenanimal = (text + x);
     };
@@ -139,16 +88,102 @@ app.controller('myCtrl', function ($scope) {
         var x, text;
         x = document.getElementById("betamount").value;
 
-        if (isNaN(x) || x < 0 || x > $scope.money) {
+        if (isNaN(x) || x < 0 || x > $scope.money || x === "" || firstprompt === 0) {
             text = "Error";
         } else {
             text = "Choice: ";
+            stored[animalposition][turn] += parseInt(x);
             $scope.money -= x;
+            node[turn].money = $scope.money;
+            firstprompt = 0;
+            if ($scope.money == 0 && $scope.name != "Banker") {
+                turn++;
+                $scope.name = node[turn].name;
+                $scope.money = node[turn].money;
+            }
+            if ($scope.name == "Banker") {
+                rollDice();
+                //console.log("node[f] has : "+node[f].money);
+                   var f;
+            for (f = 0; f < 2; f++) {
+                node[f].money += (stored[0][f] * multiply[0][f]) + (stored[1][f] * multiply[1][f]) + (stored[2][f] * multiply[2][f]) + (stored[3][f] * multiply[3][f])
+                        + (stored[4][f] * multiply[4][f]) + (stored[5][f] * multiply[5][f]);
+                console.log("node[f] has : "+node[f].money);
+            }
+            }
         }
         $scope.bet = (text + x);
-    };
+
+    }; //end of prompt2
+    var dice = [5, 5, 5]; //dice could roll in the beginning of the game hehe
+    var rollDice = function () {
+        
 
 
+//return money
+        for (m = 0; m < 3; m++) {
+console.log("the dice is : "+dice[m]);
+            switch (dice[1]) {
+                case 0:
+                    returnMoney(m, 0);
+                    break;
+                case 1:
+                    returnMoney(m, 1);
+                    break;
+                case 2:
+                    returnMoney(m, 2);
+                    break;
+                case 3:
+                    returnMoney(m, 3);
+                    break;
+                case 4:
+                    returnMoney(m, 4);
+                    break;
+                case 5:
+                    returnMoney(m, 5);
+                    break;
+                default:
+
+            }
+         
+        }//end of for loop
+  }; // end of rollDice function
+
+        function returnMoney(n, k) {
+            console.log("entered returnMoney function");
+            for (j = 0; j < 2; j++) {
+                multiply[k][j]++;
+                console.log("multiply array is type: "+typeof multiply[k][j] + multiply[k][j]);
+            }
+            if (n == 1) {
+                if (dice[n] != dice[0]) {
+                    allocateMoney(k);
+                }
+            } else if (n == 2) {
+                if ((dice[n] != dice[1]) && (dice[n] != dice[0])) {
+                    allocateMoney(k);
+                }
+            } else {
+                allocateMoney(k);
+
+console.log("*entered the else returMoney");
+            }
+
+        } // returnedMoney function ended
+
+        function allocateMoney(k) {
+            var z;
+            for (z = 0; z < 2; z++) {
+                //add money
+                console.log("stored[kz] has value: "+stored[k][z]);
+                node[z].money += stored[k][z];
+                bankerfee += stored[k][z];
+                console.log("the banker will pay : "+bankerfee);
+            }
+         
+
+        }
+  
 });//end of controller
 
 
